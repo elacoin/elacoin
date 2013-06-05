532015f6adb55aded31e6933ba5b38d80f149fc0
Alex Jones <admin@zpxhost.com>
2013-06-05 12:09:02 -0400

    Adding checkpoint at block 19200

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -27,6 +27,7 @@ namespace Checkpoints
         (     0, uint256("0x521d2bc9fdb2534f26e5df423db7a64da14892993e4b4f0329c829a5e8194305"))
         (  2500, uint256("0xd301c065649cbbfb1e93cec353047a12256365c9d50ac689b23ad913df25b6fd"))
         (  5000, uint256("0x7671812122b751155ca6ed038f7b48e0f4f98a27a40dc9521d1c61d98e587544"))
+        ( 19200, uint256("0x0d0ce80dfac71b50c597a72bc4ca23f4c8ced8e6c3276716a01ba9c80b353c1d"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
