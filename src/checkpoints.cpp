c2a311c5257461fbb2646b2c6c0303730762ee07
math <math@ire.sizone.org>
2013-06-09 22:36:59 -0400

    checkpoint 21600 d464161f345ccc1324912ee207db807d2e404f681e2020bcd55ee9f535c0093d -mat5x

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -31,6 +31,7 @@ namespace Checkpoints
     ( 19350, uint256("0xc6c725fb3c376a2cc4a0796ab4aa04af1ff472628c8e82ece55657b60b3150a5"))
     ( 19400, uint256("0xce70abf9c0930d5cdcff555ceec6e6777f70f3a8bbc4b1efc9ac5c49a3b6c8e6"))
     ( 20940, uint256("0x10478476fb1dc7533d9814e23329633d9758e80a3e2c4b61c6e95778a00e1375"))
+    ( 21600, uint256("0xd464161f345ccc1324912ee207db807d2e404f681e2020bcd55ee9f535c0093d"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
